90de0854ea451a2226596de83eb50ca77a21b292
Khalid Imran <ekhalid090@gmail.com>
2024-09-28 00:19:21 +0600

    snake game logic done

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 let gameBoard = document.querySelector('.game-board');
-let currentScore = document.querySelector('.current');
-let highScore = document.querySelector('.high');
+let gameHomeScreen = document.querySelector('.game-home-screen');
+let gameOverScreen = document.querySelector('.game-over');
+let currentScoreEl = document.querySelector('.current');
+let highScoreEl = document.querySelector('.high');
 
-let gridSize = 25;
+let gridSize = 20;
 let snake = [{x: 10, y: 10}];
 let food = generateFood();
 let direction = 'right';
 let started = false;
 let gameInterval = null;
-let snakeSpeed = 1000;
+let snakeSpeed = 200;
+let currentScore = 0;
+let highScore = 0;
 
 function drawGame() {
     gameBoard.innerHTML = '';
@@ -63,12 +67,14 @@ function moveSnake() {
     }
 
     if (snakeHead.x === food.x && snakeHead.y === food.y) {
-        food = generateFood()
+        food = generateFood();
         clearInterval(gameInterval);
         gameInterval = setInterval(() => {
             moveSnake();
             drawGame();
-        }, snakeSpeed)
+        }, snakeSpeed);
+        currentScore++;
+        currentScoreEl.textContent = currentScore;
     } else {
         snake.pop();
     }
@@ -104,6 +110,8 @@ function handleKeyPress(event) {
 }
 
 function startGame() {
+    gameHomeScreen.style.display = 'none';
+    gameBoard.style.display = 'grid';
     started = true;
     snake = [{x: 10, y: 10}];
     gameInterval = setInterval(() => {
@@ -113,8 +121,38 @@ function startGame() {
 }
 
 function stopGame() {
-    started = false;
     clearInterval(gameInterval);
+    gameBoard.innerHTML = '';
+    gameOverScreen.classList.add('active');
+    updateHighScore();
+    setTimeout(() => {
+        started = false;
+        food = generateFood();
+        gameHomeScreen.style.display = 'flex';
+        gameBoard.style.display = 'none';
+        gameOverScreen.classList.remove('active');
+    }, 2500)
 }
+
+function updateHighScore() {
+    let  highScore = localStorage.getItem('highScore');
+    if (highScore != null) {
+        highScore = JSON.parse(highScore);
+    }
+    if (currentScore > highScore) {
+        highScore = currentScore;
+    }
+    localStorage.setItem('highScore', JSON.stringify(highScore));
+    showHighScore()
+    currentScore = 0;
+    currentScoreEl.textContent = currentScore;
+}
+function showHighScore() {
+    let  highScore = localStorage.getItem('highScore');
+    if (highScore != null) {
+        highScoreEl.textContent = highScore;
+    }
+}
+showHighScore();
 
 document.addEventListener('keydown', handleKeyPress);
